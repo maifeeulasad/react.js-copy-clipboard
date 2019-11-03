f7b41f59f2b925dab1213caae781941fb35c13b8
Maifee Ul Asad <maifeeulasad@gmail.com>
2019-11-03 16:08:32 +0600

    //

--- copy-clipboard/index.js
+++ copy-clipboard/index.js
@@ -1,9 +1,9 @@
-exports.copyText = function(copyText) {
-    if(copyText===null || copyText===undefined || copyText==="")
+exports.copyText = function(textT) {
+    if(textT===null || textT===undefined || textT==="")
         return;
     var tempInput = document.createElement('INPUT');
     document.body.appendChild(tempInput);
-    tempInput.setAttribute('value', copyText);
+    tempInput.setAttribute('value', textT);
     tempInput.select();
     document.execCommand('copy');
     document.body.removeChild(tempInput);
