cab3dffbd5fde8bd7a960031690ebc17e496d95e
maifeeulasad <maifeeulasad@gmail.com>
2020-01-10 20:28:51 +0600

    created parameter for alert

--- copy-clipboard/index.js
+++ copy-clipboard/index.js
@@ -1,11 +1,12 @@
-exports.copyText = function(textT) {
-    if(textT===null || textT===undefined || textT==="")
+exports.copyText = function(text_to_copy,alert_or_not) {
+    if(text_to_copy===null || text_to_copy===undefined || text_to_copy==="")
         return;
     var tempInput = document.createElement('INPUT');
     document.body.appendChild(tempInput);
-    tempInput.setAttribute('value', textT);
+    tempInput.setAttribute('value', text_to_copy);
     tempInput.select();
     document.execCommand('copy');
     document.body.removeChild(tempInput);
-    alert("Text copied in clipboard");
+    if(alert_or_not)
+	    alert("Text copied in clipboard:\n"+len(text_to_copy)>30 ?text_to_copy.substring(0,30) : text_to_copy);
 }
