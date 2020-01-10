61a7260ab403b08403f9865d505347e18704ebd4
maifeeulasad <maifeeulasad@gmail.com>
2020-01-10 20:35:26 +0600

    no len fixed

--- copy-clipboard/index.js
+++ copy-clipboard/index.js
@@ -1,6 +1,8 @@
 exports.copyText = function(text_to_copy,alert_or_not) {
     if(text_to_copy===null || text_to_copy===undefined || text_to_copy==="")
         return;
+    if(typeof alert_or_not == undefined)
+    	alert_or_not=false;
     var tempInput = document.createElement('INPUT');
     document.body.appendChild(tempInput);
     tempInput.setAttribute('value', text_to_copy);
@@ -8,5 +10,5 @@ exports.copyText = function(text_to_copy,alert_or_not) {
     document.execCommand('copy');
     document.body.removeChild(tempInput);
     if(alert_or_not)
-	    alert("Text copied in clipboard:\n"+len(text_to_copy)>30 ?text_to_copy.substring(0,30) : text_to_copy);
+	    alert("Text copied in clipboard:\n"+text_to_copy.length>30 ?text_to_copy.substring(0,30) : text_to_copy);
 }
