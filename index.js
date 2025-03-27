032c8084a413d7245c2146cc77b3d785502c6629
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-27 14:30:16 +0600

    [fix,doc]: corrected jsdoc, and implementation;

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 /**
-  * Creates a temporary input element in the DOM.
-  * This can be used for various purposes, such as copying text to the clipboard.
-  * 
-  * @type {HTMLInputElement} The temporary input element created.
-  */
-exports.copyText = function (text_to_copy, alert_or_not) {
-    if (text_to_copy === null || text_to_copy === undefined || text_to_copy === "")
-        return;
-    if (typeof alert_or_not == undefined)
-        alert_or_not = false;
-    var tempInput = document.createElement('INPUT');
+ * Creates a temporary input element in the DOM to copy text to clipboard
+ * @param {string} text_to_copy - Text to be copied to clipboard
+ * @param {boolean} [alert_or_not=false] - Whether to show alert after copying
+ */
+exports.copyText = function (text_to_copy, alert_or_not = false) {
+    if (!text_to_copy) return;
+
+    /** @type {HTMLInputElement} */
+    const tempInput = document.createElement('input');
     document.body.appendChild(tempInput);
-    tempInput.setAttribute('value', text_to_copy);
+    tempInput.value = text_to_copy;
     tempInput.select();
-    // NOTE: no alternative to execCommand for copy to clipboard so far
-    // ref: https://stackoverflow.com/a/70831583/10305444
+    
     document.execCommand('copy');
     document.body.removeChild(tempInput);
-    if (alert_or_not)
-        alert("Text copied in clipboard:\n" + text_to_copy.length > 30 ? text_to_copy.substring(0, 30) : text_to_copy);
-}
+
+    if (alert_or_not) {
+        const alertText = text_to_copy.length > 30 
+            ? text_to_copy.substring(0, 30) + '...' 
+            : text_to_copy;
+        alert("Text copied in clipboard:\n" + alertText);
+    }
+};
