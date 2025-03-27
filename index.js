1f270b957089470b3016d81738df9cb23bd59cf4
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-27 14:18:23 +0600

    [lint]: linting all files;

--- index.js
+++ index.js
@@ -4,17 +4,17 @@
   * 
   * @type {HTMLInputElement} The temporary input element created.
   */
-exports.copyText = function(text_to_copy,alert_or_not) {
-    if(text_to_copy===null || text_to_copy===undefined || text_to_copy==="")
+exports.copyText = function (text_to_copy, alert_or_not) {
+    if (text_to_copy === null || text_to_copy === undefined || text_to_copy === "")
         return;
-    if(typeof alert_or_not == undefined)
-    	alert_or_not=false;
+    if (typeof alert_or_not == undefined)
+        alert_or_not = false;
     var tempInput = document.createElement('INPUT');
     document.body.appendChild(tempInput);
     tempInput.setAttribute('value', text_to_copy);
     tempInput.select();
     document.execCommand('copy');
     document.body.removeChild(tempInput);
-    if(alert_or_not)
-	    alert("Text copied in clipboard:\n"+text_to_copy.length>30 ?text_to_copy.substring(0,30) : text_to_copy);
+    if (alert_or_not)
+        alert("Text copied in clipboard:\n" + text_to_copy.length > 30 ? text_to_copy.substring(0, 30) : text_to_copy);
 }
